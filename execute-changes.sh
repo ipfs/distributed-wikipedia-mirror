d9103896802cf662ed1edefca65fc78114b394df
Jakub Sztandera <kubuxu@protonmail.ch>
2017-05-11 17:27:50 +0200

    Make the execute-changes.sh more usabe

--- execute-changes.sh
+++ execute-changes.sh
@@ -1,38 +1,104 @@
 #!/bin/bash
+# vim: set ts=2 sw=2:
+
 set -euo pipefail
 IFS=$'\n\t'
-ROOT="/root"
 
-IPNS_HASH="QmVH1VzGBydSfmNG7rmdDjAeBZ71UVeEahVbNpFQtwZK8W"
-SNAP_DATE="2017-04"
-
-ipfs-replace() {
-  ipfs files rm "$ROOT/$2" || true
-  ipfs files cp "$1" "$ROOT/$2"
+error() {
+	echo "$@"
+	exit 1
 }
 
-ipfs files rm "$ROOT/-/j/body.js" 
-ipfs files cp /ipfs/$(sed -e 's/{{SNAPSHOT_DATE}}/'"$SNAP_DATE"'/g' -e 's/{{IPNS_HASH}}/'"$IPNS_HASH"'/g' \
-	scripts/body.js | ipfs add -Q) "$ROOT/-/j/body.js" 
-ipfs files rm "$ROOT/I/s/Wikipedia-logo-v2-200px-transparent.png"
-ipfs files cp /ipfs/$(ipfs add -Q assets/wikipedia-on-ipfs-small-flat-cropped-offset-min.png) "$ROOT/I/s/Wikipedia-logo-v2-200px-transparent.png"
-ipfs files rm "$ROOT/I/s/wikipedia-on-ipfs.png" || true
-ipfs files cp /ipfs/$(ipfs add -Q assets/wikipedia-on-ipfs-100px.png) "$ROOT/I/s/wikipedia-on-ipfs.png"
+usage() {
+	echo "USAGE:"
+	echo "	$0 [-h|--help] [--ipns=<ipns hash>] [--date=<date of snapshot>]";
+	echo "		[--search] <ipfs files root>"
+	echo ""
+	echo "	-h|--help		- displays help"
+	echo "	--ipns			- ipns hash of the archive"
+	echo "	--date			- date of snapshot (defaults to this month)"
+	echo "  --search		- hash of search IPLD structure"
+	exit 2
+}
 
-ipfs files rm "$ROOT/-/j/search.js" || true
-ipfs files cp /ipfs/$(ipfs add -Q scripts/search.js) "$ROOT/-/j/search.js"
+if [ "$(getopt --test >/dev/null 2>&1; echo $?)" -ne "4" ]; then
+	error "getopt enchanced required, 'getopt --test' should have exit code 4"
+fi
 
 
+LONG_OPT="help,search:,ipns:,date:"
+SHORT_OPT="h"
+PARSED_OPTS=$(getopt -n "$0" -o "$SHORT_OPT" -l "$LONG_OPT" -- "$@") || usage
+
+eval set -- "$PARSED_OPTS"
+
+# defaults
+SNAP_DATE=$(date +"%Y-%m-%d")
+IPNS_HASH=""
+SEARCH=""
+
+while true; do
+	case "$1" in
+		-h|--help)
+			usage;;
+		--date)
+			SNAP_DATE="$2"
+			shift 2;;
+		--ipns)
+			IPNS_HASH="$2"
+			shift 2;;
+		--search)
+			SEARCH="$2"
+			shift 2;;
+		--)
+			shift;
+			break;;
+	esac
+done
+
+if [ -z "${1-}" ]; then
+	echo "Missing ipfs files root"
+	usage
+fi
+ROOT="$1"
+
+ipfs-replace() {
+	ipfs files rm "$ROOT/$1" >/dev/null 2>&1 || true
+	ipfs files cp "$2" "$ROOT/$1"
+}
+
+if ipfs files stat "$ROOT/A" >/dev/null 2>&1; then
+	ipfs files mv "$ROOT/A" "$ROOT/wiki"
+fi
+
+NEW_BODYJS=$(
+	sed -e 's/{{SNAPSHOT_DATE}}/'"$SNAP_DATE"'/g' \
+	-e 's/{{IPNS_HASH}}/'"$IPNS_HASH"'/g' scripts/body.js |\
+	if [ -n "$SEARCH" ]; then
+		cat - <(sed -e 's/{{SEARCH_CID}}/'"$SEARCH"'/' scripts/search-shim.js)
+	fi | ipfs add -Q
+)
+
+ipfs-replace "-/j/body.js" "/ipfs/$NEW_BODYJS"
+ipfs-replace "I/s/Wikipedia-logo-v2-200px-transparent.png" \
+	"/ipfs/$(ipfs add -q assets/wikipedia-on-ipfs-small-flat-cropped-offset-min.png)"
+ipfs-replace "I/s/wikipedia-on-ipfs.png" \
+	"/ipfs/$(ipfs add -Q assets/wikipedia-on-ipfs-100px.png)"
+
+if [ -n "$SEARCH" ]; then
+	ipfs-replace "-/j/search.js" "/ipfs/$(ipfs add -Q scripts/search.js)"
+fi
 
 # comment out some debug stuff in head.js
 HEAD_JS_LOCATION="$(ipfs files stat --hash "$ROOT")/-/j/head.js"
 HEAD_JS_HASH="$(ipfs cat "$HEAD_JS_LOCATION" | sed -e "s|^\tdocument.getElementsByTagName( 'head' )|//\0|" | ipfs add -Q)"
 
-ipfs files rm "$ROOT/-/j/head.js"
-ipfs files cp "/ipfs/$HEAD_JS_HASH" "$ROOT/-/j/head.js"
+ipfs-replace "-/j/head.js" "/ipfs/$HEAD_JS_HASH"
 
-ipfs-replace /ipfs/$(ipfs add -Q redirect-page/index.html) "/wiki/index.html"
-ipfs-replace /ipfs/$(ipfs add -Q redirect-page/index_root.html) "/index.html"
+ipfs-replace "/wiki/index.html" "/ipfs/$(ipfs add -Q redirect-page/index.html)"
+ipfs-replace "/index.html" "/ipfs/$(ipfs add -Q redirect-page/index_root.html)"
 
+echo "We are done !!!"
 ipfs files stat "$ROOT"
+
 
---
+++ scripts/search-shim.js
@@ -0,0 +1,118 @@
+const searchInject = `
+<div class="search-root">
+	<form class="search-form">
+		<input class="search-text" placeholder="Vikipedi üzerinde ara" type="text" />
+		<input type="submit" class="search-btn" />
+	</form>
+
+
+	<div class="search-container">
+	<a class="search-close">[X]</a>
+	<div class="search-results"></div>
+	</div>
+
+	<div id="entry-proto" style="display: none;">
+		<div class="entry">
+			<span class="entry-id"></span>
+			<a class="entry-link"></a>
+		</div>
+	</div>
+
+	<style>
+		.search-root {
+			margin-left: 60%;
+		}
+		.search-form {
+			position: relative;
+		}
+		.search-text {
+			width: 100%;
+		}
+		.search-container {
+			position: relative;
+		}
+		.search-close {
+			display: none;
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+		.search-btn {
+			position: absolute;
+			top: 0;
+			right: 0;
+			margin: 0;
+			padding: 0;
+			border: 0;
+			width: 1.65em;
+			height: 100%;
+			background-image: url("../I/s/search-ltr.png");
+			background-color: transparent;
+			background-position: center center;
+			background-repeat: no-repeat;
+			text-indent: -99999px;
+			overflow: hidden;
+			direction: ltr;
+			white-space: nowrap;
+			cursor: pointer;
+		}
+
+		.entry-id {
+			display: none;
+		}	
+	</style>
+</div>
+`
+var script = document.createElement('script');
+script.src = "../-/j/search.js";
+
+document.head.appendChild(script);
+
+var anch = document.querySelector('#top');
+
+var div = document.createElement('div');
+div.innerHTML = searchInject;
+anch.parentNode.insertBefore(div.firstElementChild, anch.nextSibling);
+
+var closeBtn = document.querySelector('.search-close')
+
+closeBtn.onclick = function() {
+	delResults();
+	closeBtn.style.display = "none";
+};
+
+
+var delResults = function() {
+		var resultsElem = document.querySelector('.search-results');
+		while(resultsElem.firstChild) resultsElem.removeChild(resultsElem.firstChild);
+}
+
+var wikiSearch = new WikiSearch('{{SEARCH_CID}}');
+
+document.querySelector('.search-form').onsubmit = function() {
+	if (search == null) {
+		return false;
+	}
+	var val = document.querySelector('.search-text').value;
+	wikiSearch.search(val).then(function(results) {
+		var proto = document.getElementById('entry-proto').children[0];
+		var resultsElem = document.querySelector('.search-results');
+		delResults()
+
+		results.forEach(function(art, idx) {
+			var entry = proto.cloneNode(true);
+			entry.getElementsByClassName('entry-id')[0].appendChild(document.createTextNode(idx));
+			var link = entry.getElementsByClassName('entry-link')[0];
+			link.appendChild(document.createTextNode(art.replace(/_/g, " ").slice(0, -5)));
+			link.href = art;
+			closeBtn.style.display = "inherit";
+
+			resultsElem.appendChild(entry);
+		})
+
+	});
+	return false
+};
+
+
+
