bb9f48c47de62ffe2548faaa72bef009282048e1
Marcin Rataj <lidel@lidel.org>
2020-01-22 19:07:18 +0100

    feat: tools for finding original main page name
    tools/find_main_page_name.sh:
     Every Wikipedia version uses different name of the main page,
     this scripts takes language code and returns the filename
    
    tools/find_original_main_page_url.sh:
     Landing pages shipping with ZIM file are either truncated or
     Kiwix-specific.
     This script finds the URL of original version of the langing page
     mathing the timestamp of snapshot in unpacked ZIM directory
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

---
+++ tools/find_main_page_name.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vim: set ts=2 sw=2:
+
+set -euo pipefail
+
+# Every Wikipedia version uses different name of the main page
+
+usage() {
+	echo "USAGE:"
+	echo "	$0 <lang code>";
+	echo ""
+	exit 2
+}
+
+if [ -z "${1-}" ]; then
+	echo "Missing language code"
+	usage
+fi
+
+MAIN_PAGE=$(curl -Ls -o /dev/null -w %{url_effective} https://${1}.wikipedia.org | cut -d"/" -f5)
+echo -n "${MAIN_PAGE}.html"
